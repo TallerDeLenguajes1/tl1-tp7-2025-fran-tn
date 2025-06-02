522793f17bdd0bda4b4357ca744256a09356ecfb
fran-tn <francisconun8mail.com>
2025-06-02 12:35:18 -0300

    se completo el ejercicio2

--- ejercicio2/Empleado.cs
+++ ejercicio2/Empleado.cs
@@ -1,11 +1,68 @@
 namespace EspacioEmpleado;
 
+ public class Salario
+    {
+        private double sueldo;
+
+        private double adicional;
+
+        public double Sueldo
+        {
+            get => sueldo;
+        }
+
+        public double Adicional
+        {
+            get => adicional;
+        }
+
+        public void cargarSueldo(double cargar)
+        {
+            sueldo = cargar;
+        }
+
+        public void calcularPorTiempo(int anio)
+        {
+            adicional += (sueldo * 0.01 * anio);
+        }
+
+        public void calcularPorRol(string ingresarRol)
+        {
+            switch (ingresarRol)
+            {
+                case "Ingeniero":
+                case "Especialista":
+                    adicional += (adicional * 0.5);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void calcularPorEstadoCivil(int estado)
+        {
+            switch (estado)
+            {
+                case 1:
+                    adicional += 150000;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
 public class Empleado
 {
     private int antiguedad; //declaro las variables privadas
+
     private int edad;
 
-    private bool jubilado;
+    private string rol = "";
+
+    private int casado;
+
+    private Salario infoSalario = new Salario();
 
     public int Antiguedad   //Declaro variables que me permitiran obtener las privadas
     {
@@ -17,7 +74,24 @@ public class Empleado
         get => edad;
     }
 
-    public void CargarAntiguedad(int cargar)
+    public string Rol
+    {
+        get => rol;
+    }
+
+    public int Casado
+    {
+        get => casado;
+    }
+
+    public Salario InfoSalario
+    {
+        get => infoSalario;
+    }
+
+   
+
+    public void CargarAntiguedad(int cargar) //Esto me permite cargar las variables
     {
         antiguedad = cargar;
     }
@@ -25,6 +99,16 @@ public class Empleado
     public void CargarEdad(int cargar)
     {
         edad = cargar;
+    }
+
+    public void cargarRol(string cargar)
+    {
+        rol = cargar;
+    }
+
+    public void cargarCasado(int cargar)
+    {
+        casado = cargar;
     }
 
 }
--- ejercicio2/Program.cs
+++ ejercicio2/Program.cs
@@ -2,8 +2,9 @@
 using EspacioEmpleado;
 
 Empleado[] lista = new Empleado[3];
-string recibirDato;
-int edad, antiguedad;
+string recibirDato = "", cargo;
+int edad, antiguedad, estadoCivil, mostrar = -1, auxiliar = 0;
+double sueldo, total = 0;
 
 for (int i = 0; i < 3; i++)
 {
@@ -26,13 +27,84 @@ for (int i = 0; i < 3; i++)
         recibirDato = Console.ReadLine();
     }
 
+    Console.WriteLine("Ingrese el salario del empleado");
+    recibirDato = Console.ReadLine();
+
+    while (!(double.TryParse(recibirDato, out sueldo)))
+    {
+        Console.WriteLine("Ingrese el salario del empleado");
+        recibirDato = Console.ReadLine();
+    }
+
+    Console.WriteLine("Ingrese el cargo del empleado");
+    cargo = Console.ReadLine();
+
+    Console.WriteLine("El empleado se encuentra casado?");
+    Console.WriteLine("1)SI");
+    Console.WriteLine("0)NO");
+    recibirDato = Console.ReadLine();
+
+
+    while (!(int.TryParse(recibirDato, out estadoCivil)))
+    {   
+            Console.WriteLine("El empleado se encuentra casado?");
+            Console.WriteLine("1)SI");
+            Console.WriteLine("0)NO");
+            recibirDato = Console.ReadLine();    
+    }
+
+    if (estadoCivil != 1)
+    {
+        estadoCivil = 0;
+    }
+
 
     lista[i] = new Empleado();
     lista[i].CargarEdad(edad);
     lista[i].CargarAntiguedad(antiguedad);
+    lista[i].cargarRol(cargo);
+    lista[i].cargarCasado(estadoCivil);
+    lista[i].InfoSalario.cargarSueldo(sueldo);
+    lista[i].InfoSalario.calcularPorTiempo(antiguedad);
+    lista[i].InfoSalario.calcularPorRol(cargo);
+    lista[i].InfoSalario.calcularPorEstadoCivil(estadoCivil);
 }
 
-for(int i = 0; i < 3; i++)
+for (int i = 0; i < 3; i++)
 {
+    total = total + lista[i].InfoSalario.Sueldo + lista[i].InfoSalario.Adicional;
+    if (lista[i].Edad < 65 && lista[i].Edad > auxiliar)
+    {
+        auxiliar = lista[i].Edad;
+        mostrar = i;
+    }
+
+   /* Console.WriteLine("~~~~~~~~~~~~AAAAAA~~~~~~");
     Console.WriteLine($"Edad: {lista[i].Edad}, Antiguedad: {lista[i].Antiguedad}");
+
+    Console.WriteLine($"Años para jubilarse: {65 - lista[i].Edad}");
+
+    Console.WriteLine($"Salario: {lista[i].InfoSalario.Sueldo}, adicional: {lista[i].InfoSalario.Adicional}");
+    Console.WriteLine($"Rol: {lista[i].Rol}");
+    Console.WriteLine($"Esta casado: {lista[i].Casado}");
+   */
+
+
+}
+
+if (mostrar != -1)
+{
+    Console.WriteLine("~~~~~~~~~~~~~~~~~~");
+    Console.WriteLine($"Edad: {lista[mostrar].Edad}, Antiguedad: {lista[mostrar].Antiguedad}");
+
+    Console.WriteLine($"Años para jubilarse: {65 - lista[mostrar].Edad}");
+    
+    Console.WriteLine($"Salario: {lista[mostrar].InfoSalario.Sueldo}, Monto a cobrar: {lista[mostrar].InfoSalario.Sueldo + lista[mostrar].InfoSalario.Adicional}");
+
+    Console.WriteLine($"Dinero gastado en sueldos: {total}");
+
+}
+else
+{
+    Console.WriteLine("Los  empleados no cumplen los requisitos");
 }
