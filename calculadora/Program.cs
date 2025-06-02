b96e50dbfc053d29b9b4b57ea32e58931fea580b
fran-tn <francisconun8mail.com>
2025-06-01 21:35:47 -0300

    Se completo el ejercicio1

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,59 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System;
 using EspacioCalculadora;
 
-int iterar = 1, operacion;
-double num2;
-string auxiliar;
-Calculadora Operaciones = new Calculadora();
+
+        int iterar = 1, operacion;
+        double num2;
+        string auxiliar;
+        Calculadora Operaciones = new Calculadora();
 
 
-while(iterar == 1)
-{
-   
-    
-    Console.WriteLine("Seleccione una operacion");
-    Console.WriteLine("1)SUMA");
-    Console.WriteLine("2)RESTA");
-    Console.WriteLine("3)PRODUCTO");
-    Console.WriteLine("4)DIVISION");
-    Console.WriteLine("0)SALIR");
-    auxiliar = Console.ReadLine();
+        while (iterar == 1)
+        {
+            Console.WriteLine("Seleccione una operacion");
+            Console.WriteLine("1)SUMA");
+            Console.WriteLine("2)RESTA");
+            Console.WriteLine("3)PRODUCTO");
+            Console.WriteLine("4)DIVISION");
+            Console.WriteLine("0)SALIR");
+            auxiliar = Console.ReadLine();
+            
 
-    operacion = int.TryParse(auxiliar, out operacion);
+            while (!int.TryParse(auxiliar, out operacion))
+        {
+                Console.WriteLine("Seleccione una operacion");
+                Console.WriteLine("1)SUMA");
+                Console.WriteLine("2)RESTA");
+                Console.WriteLine("3)PRODUCTO");
+                Console.WriteLine("4)DIVISION");
+                Console.WriteLine("0)SALIR");
+                auxiliar = Console.ReadLine();
+        }
 
-    Console.WriteLine("Ingrese un numero");
-    auxiliar = Console.ReadLine();
-    num2 = double.TryParse(auxiliar, out num2);
 
+        if (operacion != 0)
+        {
+                Console.WriteLine("Ingrese un numero");
+                auxiliar = Console.ReadLine();
 
-    switch(operacion)
-    {
-        case 1: Operaciones.Suma(num2);
-                Console.WriteLine($"Reultado: {Operaciones.Resultado}");
-        break;
-
-        case 2: Operaciones.Restar(num2);
-                Console.WriteLine($"Reultado: {Operaciones.Resultado}");
-        break;
-
-        case 3: Operaciones.Multiplicar(num2);
-                Console.WriteLine($"Reultado: {Operaciones.Resultado}");
-        break;
-
-        case 4: if(num2 != 0)
+                while (!double.TryParse(auxiliar, out num2))
                 {
-                    Operaciones.Dividir(num2);
-                    Console.WriteLine($"Resultado: {Operaciones.Resultado}");
-                } else {
-                    Console.WriteLine("No se puede dividir por 0");
+                        Console.WriteLine("Ingrese un numero");
+                        auxiliar = Console.ReadLine();
                 }
-        break;
 
-        default: iterar = 0;
+
+                switch (operacion)
+                {
+                        case 1:
+                                Operaciones.Suma(num2);
+                                Console.WriteLine($"Reultado: {Operaciones.Resultado}");
+                                break;
+
+                        case 2:
+                                Operaciones.Restar(num2);
+                                Console.WriteLine($"Reultado: {Operaciones.Resultado}");
+                                break;
+
+                        case 3:
+                                Operaciones.Multiplicar(num2);
+                                Console.WriteLine($"Reultado: {Operaciones.Resultado}");
+                                break;
+
+                        case 4:
+                                if (num2 != 0)
+                                {
+                                        Operaciones.Dividir(num2);
+                                        Console.WriteLine($"Resultado: {Operaciones.Resultado}");
+                                }
+                                else
+                                {
+                                        Console.WriteLine("No se puede dividir por 0");
+                                }
+                                break;
+
+                        default:
+                                Console.WriteLine("Opcion incorrecta");
+                                break;
+                }
+        }
+        else
+        {
+                iterar = 0;
                 Operaciones.Limpiar();
-        break;
-    }
+        }
+        
 
 
-}
-
+        }
+    
